575be3c818e096f369b4e50e48b3533c35beecb2
MrKabado <jersonjaybonghanoy@gmail.com>
2024-12-11 23:21:05 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+fetch('https://official-joke-api.appspot.com/random_joke', {method: 'GET'})
+.then(x => x.json())
+.then(y => myFunction(y));
+
+function myFunction(y) {
+
+    let jokeDis = document.getElementById('joke-dis'); //joke discription
+    let jokeAns = document.getElementById('joke-ans'); //joke punchline
+
+    let btnAns = document.getElementById('btn-answer'); //button for submit
+    let btnRef = document.getElementById('btn-refresh'); //button for refresh
+
+    jokeDis.innerHTML = y.setup;
+
+    btnAns.onclick = function() {
+        jokeAns.innerHTML = y.punchline;
+    }
+
+    btnRef.onclick = function() {
+        location.reload();
+    }
+}
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+     navigator.serviceWorker.register('../sw.js').then( () => {
+      console.log('Service Worker Registered')
+     })
+   })
+  }
